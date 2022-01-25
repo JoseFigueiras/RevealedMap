99fc469ef8fc841e3199763e3b87d8ea51f38913
JoseFigueiras <66280179+JoseFigueiras@users.noreply.github.com>
2022-01-25 16:35:35 +0000

    initial commit

---
+++ samples/Pools/script.cpp
@@ -0,0 +1,29 @@
+/*
+	THIS FILE IS A PART OF RDR 2 SCRIPT HOOK SDK
+				http://dev-c.com
+			(C) Alexander Blade 2019
+*/
+
+#include "script.h"
+#include <string>
+#include <vector>
+
+void update()
+{
+	MAP::SET_MINIMAP_HIDE_FOW(true);
+}
+
+void main()
+{		
+	while (true)
+	{
+		update();
+		WAIT(0);
+	}
+}
+
+void ScriptMain()
+{	
+	srand(GetTickCount());
+	main();
+}
